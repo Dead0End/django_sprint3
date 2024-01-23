f43f9b53e0bcb9b7c3fe3b40a9d905cdd1736fee
Dead0End <DeadEnd>
2024-01-24 01:15:39 +0300

    Finall task

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -8,16 +8,14 @@ from constants import MAX_RES_INDEX
 
 def index(request):
     """Главная страница, Лента записей"""
-    post_list = get_posts().order_by("-pub_date")
-    [MAX_RES_INDEX].prefetch_related('category')
-    context = {"post_list": post_list}
+    post_list = get_posts().order_by('-pub_date')[:MAX_RES_INDEX]
+    context = {'post_list': post_list}
     return render(request, 'blog/index.html', context)
 
 
 def post_detail(request, post_id):
     """Подробное описание выбранной записи"""
-    post = get_object_or_404(
-        get_posts().prefetch_related('category'), pk=post_id)
+    post = get_object_or_404(get_posts(), pk=post_id)
     context = {'post': post}
     return render(request, 'blog/detail.html', context)
 
@@ -25,7 +23,7 @@ def post_detail(request, post_id):
 def category_posts(request, category_slug):
     """Отображение публикации по категории"""
     category = get_object_or_404(get_category(), slug=category_slug)
-    post_list = get_posts().filter(category=category).order_by(
-        "-pub_date")[:MAX_RES_CATEGORY_POSTS]
+    post_list = get_posts().filter(
+        category=category).order_by('-pub_date')[:MAX_RES_CATEGORY_POSTS]
     context = {'category': category, 'post_list': post_list}
-    return render(request, 'blog/category.html', context)
+    return render(request, 'blog/category_posts.html', context)
