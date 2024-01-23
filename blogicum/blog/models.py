f17853d512509b13e8b2df11db4daeef2332b143
Dead0End <DeadEnd>
2024-01-24 00:23:38 +0300

    Finall task

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -81,7 +81,7 @@ class Post(BaseModel, BaseTitle):
     def str(self):
         return self.title
 
-    def get_published_categories(cls):
+    def get_published_categories(self, cls):
         return cls.objects.filter(
             is_published=True
         )
